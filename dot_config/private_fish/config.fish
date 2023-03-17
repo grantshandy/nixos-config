6c83b97c6e5dc4e49f360f199fc8595cdba51e74
grantshandy <granthandy@proton.me>
2023-03-16 21:25:42 -0600

    update

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,10 +1,6 @@
 if status is-interactive
-    set -g EDITOR helix
     set fish_greeting
-
     alias hx="helix"
-
-    if set -q "$DISPLAY"
-        exec sway
-    end
 end
+
+set -gx EDITOR helix
