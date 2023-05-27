bb077b83e19f5146efc2f8dcc13cb35db5a8e254
grantshandy <granthandy@proton.me>
2023-05-26 18:34:54 -0600

    update

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -4,3 +4,8 @@ if status is-interactive
 end
 
 set -gx EDITOR helix
+set -gx GRIM_DEFAULT_PATH ~/pics/screenshots/
+set -gx XDG_SCREENSHOTS_DIR ~/pics/screenshots/
+
+fish_add_path -a ~/.cargo/bin
+fish_add_path -a /usr/lib/emscripten
