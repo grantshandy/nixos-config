2a79679606022d7027c1b8a85c11f4fe90702a3f
grantshandy <granthandy@proton.me>
2024-09-23 08:51:12 +0900

    mount shared disk with permissions

--- shared.nix
+++ shared.nix
@@ -3,6 +3,6 @@
     {
       device = "/dev/disk/by-uuid/C62F-33E3";
       fsType = "auto";
-      options = [ "nosuid" "nodev" "nofail" "x-gvfs-show" ];
+      options = [ "nosuid" "nodev" "nofail" "x-gvfs-show" "uid=1000" "gid=1000" "umask=0000" ];
     };
 }
