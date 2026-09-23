8bbce882329a0388e2042a16f39e97fb2b9250cb
grantshandy <granthandy@proton.me>
2026-09-23 10:52:29 -0600

    Set tailscale dns in SSH config

--- modules/nixos/desktop/tailscale-bookmarks.nix
+++ modules/nixos/desktop/tailscale-bookmarks.nix
@@ -9,9 +9,18 @@
     lib.filterAttrs
     (name: h: name != thisHost && h.ssh.enable && h.tailscale.enable)
     config.network.hosts;
-  bookmarkFor = name: h: "sftp://${name}.${tailnetDomain}:${toString h.ssh.port}/home/${config.identity.user.name}/ ${name}";
+
+  bookmarkFor = name: h: "sftp://${name}/home/${config.identity.user.name}/ ${name}";
+
+  sshMatchBlockFor = name: h: {
+    hostname = "${name}.${tailnetDomain}";
+    port = h.ssh.port;
+  };
 in {
   home-manager.sharedModules = [
-    {gtk.gtk3.bookmarks = lib.mapAttrsToList bookmarkFor otherHosts;}
+    {
+      gtk.gtk3.bookmarks = lib.mapAttrsToList bookmarkFor otherHosts;
+      programs.ssh.matchBlocks = lib.mapAttrs sshMatchBlockFor otherHosts;
+    }
   ];
 }
